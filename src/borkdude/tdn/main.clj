0b9a457f6dca1c8d934090a3bcb1e40f7fc3f765
Michiel Borkent <michielborkent@gmail.com>
2021-03-04 15:25:37 +0100

    wip

--- src/borkdude/tdn/main.clj
+++ src/borkdude/tdn/main.clj
@@ -1,6 +1,46 @@
 (ns borkdude.tdn.main
   (:require [clojure.java.io :as io]
-            [clojure.tools.deps.alpha :as deps])
+            [clojure.tools.deps.alpha :as deps]
+            [clojure.tools.deps.alpha.util.io :refer [printerrln]])
+  (:import
+    ;; maven-resolver-api
+    [org.eclipse.aether RepositorySystem RepositorySystemSession DefaultRepositoryCache DefaultRepositorySystemSession ConfigurationProperties]
+    [org.eclipse.aether.artifact Artifact DefaultArtifact]
+    [org.eclipse.aether.repository LocalRepository Proxy RemoteRepository RemoteRepository$Builder]
+    [org.eclipse.aether.graph Dependency Exclusion]
+    [org.eclipse.aether.transfer TransferListener TransferEvent TransferResource]
+
+    ;; maven-resolver-spi
+    [org.eclipse.aether.spi.connector RepositoryConnectorFactory]
+    [org.eclipse.aether.spi.connector.transport TransporterFactory]
+    [org.eclipse.aether.spi.locator ServiceLocator]
+
+    ;; maven-resolver-impl
+    [org.eclipse.aether.impl DefaultServiceLocator]
+
+    ;; maven-resolver-connector-basic
+    [org.eclipse.aether.connector.basic BasicRepositoryConnectorFactory]
+
+    ;; maven-resolver-transport-file
+    [org.eclipse.aether.transport.file FileTransporterFactory]
+
+    ;; maven-resolver-transport-http
+    [org.eclipse.aether.transport.http HttpTransporterFactory]
+
+    ;; maven-aether-provider
+    [org.apache.maven.repository.internal MavenRepositorySystemUtils]
+
+    ;; maven-resolver-util
+    [org.eclipse.aether.util.repository AuthenticationBuilder DefaultProxySelector DefaultMirrorSelector]
+
+    ;; maven-core
+    [org.apache.maven.settings DefaultMavenSettingsBuilder Settings Server Mirror]
+
+    ;; maven-settings-builder
+    [org.apache.maven.settings.building DefaultSettingsBuilderFactory]
+
+    ;; plexus-utils
+    [org.codehaus.plexus.util.xml Xpp3Dom])
   (:gen-class))
 
 #_(require '[clojure.tools.deps.alpha.extensions :as ext]) ;; somehow requiring this namespace as a side effect helps...
@@ -10,8 +50,23 @@
 ;; avoid null pointer
 (mvn/make-system)
 
+(defn the-locator []
+  (let [^DefaultServiceLocator loc
+        (doto (MavenRepositorySystemUtils/newServiceLocator)
+          (.addService RepositoryConnectorFactory BasicRepositoryConnectorFactory)
+          (.addService TransporterFactory FileTransporterFactory)
+          (.addService TransporterFactory HttpTransporterFactory))]
+    (try
+      (let [c (Class/forName "clojure.tools.deps.alpha.util.S3TransporterFactory")]
+        (.addService loc TransporterFactory c))
+      (catch ClassNotFoundException _
+        (printerrln "Warning: failed to load the S3TransporterFactory class")
+        loc))))
+
 (defn -main [& args]
-  (let [arg (first args)
+  (let [l (the-locator)]
+    (alter-var-root #'clojure.tools.deps.alpha.util.maven/the-locator (constantly (delay l))))
+  (let [arg (or (first args) "{:deps {org.clojure/clojure {:mvn/version \"1.10.2\"}}}")
         edn-str (if (.exists (io/file arg))
                   (slurp arg)
                   arg)]
