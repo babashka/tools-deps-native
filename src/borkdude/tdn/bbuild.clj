354d3d3978678e0ed56aeb4a087d49e970c7f204
Michiel Borkent <michielborkent@gmail.com>
2021-11-14 17:31:12 +0100

    Fix install task by taking into account project-root

--- src/borkdude/tdn/bbuild.clj
+++ src/borkdude/tdn/bbuild.clj
@@ -1,7 +1,6 @@
 (ns borkdude.tdn.bbuild
   "Helpers for tools.bbuild"
   (:require [clojure.java.io :as jio]
-            [clojure.tools.deps.alpha.util.dir :as dir]
             [clojure.tools.deps.alpha.util.maven :as mvn])
   (:import
    [org.eclipse.aether.artifact DefaultArtifact]
@@ -11,22 +10,22 @@
 
 (defn- resolve-path
   "Adapted from tools build api"
-  ^java.io.File [path]
+  ^java.io.File [project-root path]
   (let [path-file (jio/file path)]
     (if (.isAbsolute path-file)
       ;; absolute, ignore root
       path-file
       ;; relative to *project-root*
-      (jio/file clojure.tools.deps.alpha.util.dir/*the-dir* path-file))))
+      (jio/file project-root path-file))))
 
 (defn install
   "Adapted from tools build install task"
-  [{:keys [basis lib classifier version jar-file class-dir]}]
+  [{:keys [project-root basis lib classifier version jar-file class-dir]}]
   (let [{:mvn/keys [local-repo]} basis
         group-id (namespace lib)
         artifact-id (name lib)
-        jar-file-file (resolve-path jar-file)
-        pom-dir (jio/file (resolve-path class-dir) "META-INF" "maven" group-id artifact-id)
+        jar-file-file (resolve-path project-root jar-file)
+        pom-dir (jio/file (resolve-path project-root class-dir) "META-INF" "maven" group-id artifact-id)
         pom (jio/file pom-dir "pom.xml")
         system (mvn/make-system)
         session (mvn/make-session system (or local-repo @mvn/cached-local-repo))
