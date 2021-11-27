fca55efbc26b5239fd9f406c7a31d622dd75f2a9
Michiel Borkent <michielborkent@gmail.com>
2021-11-27 18:18:29 +0100

    Use built-in clojure for Windows CI

--- script/gen-reflect-config.clj
+++ script/gen-reflect-config.clj
@@ -4,31 +4,28 @@
 ;; Runs a command to determine required config that can not be determined
 ;; statically.
 
-(require '[babashka.process :as p :refer [process]]
+(require '[babashka.tasks :refer [shell clojure]]
          '[graalvm :refer [extra-env
                            native-bin]])
 
 (def trace-agent "-agentlib:native-image-agent")
 
 (defn trace [args]
-  (let [cmd         (into
-                     ["clojure" "-Scp" (first *command-line-args*) "-M:compile"]
-                     args)
+  (let [cmd (into
+             ["-Scp" (first *command-line-args*) "-M:compile"]
+             args)
         trace-agent (str trace-agent "=trace-output=trace-file.json")]
-    (-> (process
-         cmd
-         {:extra-env (assoc extra-env
-                            "JAVA_TOOL_OPTIONS" trace-agent)})
-        p/check)))
+    (apply shell {:extra-env (assoc extra-env
+                              "JAVA_TOOL_OPTIONS" trace-agent)}
+           "bb clojure" cmd)))
 
 (defn trace->config [config-dir]
-  (-> (process
-       [(native-bin "native-image-configure")
-        "generate"
-        "--trace-input=trace-file.json"
-        (str "--output-dir=" config-dir)]
-       {:inherit true})
-      p/check))
+  (-> (shell
+       (native-bin "native-image-configure")
+       "generate"
+       "--trace-input=trace-file.json"
+       (str "--output-dir=" config-dir)
+       )))
 
 (println "Tracing")
 
