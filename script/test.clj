c21c3b09911d5d263a56301c722d8187efe7a356
Michiel Borkent <michielborkent@gmail.com>
2021-11-28 11:55:36 +0100

    Windows tests

---
+++ script/test.clj
@@ -0,0 +1,21 @@
+(ns test
+  (:require [babashka.fs :as fs]
+            [babashka.tasks :refer [shell]]
+            [graalvm :refer [windows?]]))
+
+(if (fs/exists? "tools.bbuild")
+  (do (shell {:dir "tools.bbuild"} "git checkout babashka")
+      (shell {:dir "tools.bbuild"} "git pull"))
+  (do (shell "git clone https://github.com/babashka/tools.bbuild")
+      (shell "git checkout babashka")))
+
+(def native-executable 
+  (if windows? 
+    "tools-deps-native.exe"
+    "tools-deps-native"))
+
+(fs/copy native-executable "tools.bbuild" {:replace-existing true})
+
+(shell {:dir "tools.bbuild"} "bb test")
+
+nil
